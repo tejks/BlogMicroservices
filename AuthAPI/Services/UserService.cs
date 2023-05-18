9d0ec84684e891f36c9045bc56a97f12fb0d2eb3
Tejks <sarmatys.programmer@gmail.com>
2023-05-18 22:01:14 +0200

    feat: modify user service

--- AuthAPI/Services/UserService.cs
+++ AuthAPI/Services/UserService.cs
@@ -1,19 +1,21 @@
 ﻿using AuthAPI.Dto;
 using Core.Entities.Models;
 using Core.Repositories;
+using Core.Services;
 
 namespace AuthAPI.Services;
 
-public class UserService: IUserService
+public class UserService : IUserService
 {
     private readonly IUserRepository _userRepository;
-    private readonly ILogger<UserService> _logger;
+    private readonly IPasswordService _passwordService;
     
-    public UserService(IUserRepository userRepository, ILogger<UserService> logger)
+    public UserService(IUserRepository userRepository, IPasswordService passwordService)
     {
         _userRepository = userRepository;
-        _logger = logger;
+        _passwordService = passwordService;
     }
+
     public async Task<IEnumerable<UserDto>> GetAllAsync()
     {
         var users = await _userRepository.GetAllAsync();
@@ -22,85 +24,80 @@ public class UserService: IUserService
         return usersDto;
     }
 
-    public async Task<UserDto> GetByIdAsync(Guid id)
+    public async Task<UserDto?> GetByIdAsync(Guid id)
     {
         var user = await _userRepository.GetByIdAsync(id);
         return user is null ? null : UserDto.UserToDto(user);
     }
     
-    public async Task<UserDto> GetUserByEmailAsync(string email)
+    public async Task<UserDto?> GetUserByEmailAsync(string email)
     {
         var user = await _userRepository.GetUserByEmailAsync(email);
         return user is null ? null : UserDto.UserToDto(user);
     }
 
-    public async Task<UserDto> CreateAsync(UserDto entity)
-    {
-        var user = new User()
-        {
-            Id = Guid.NewGuid(),
-            Email = entity.Email,
-            Password = entity.Password,
-            FirstName = entity.FirstName,
-            LastName = entity.LastName,
-            CreatedDate = DateTimeOffset.UtcNow
-        };
-
-        await _userRepository.CreateAsync(user);
-        var newUser = await _userRepository.GetByIdAsync(user.Id);
-
-        return UserDto.UserToDto(newUser);
-    }
-
     public async Task<UserDto> CreateAsync(UserCreateDto entity)
     {
+        var hash = _passwordService.HashPasword(entity.Password, out var salt);
+
         var user = new User()
         {
             Id = Guid.NewGuid(),
-            Email = entity.Email,
-            Password = entity.Password,
             FirstName = entity.FirstName,
             LastName = entity.LastName,
+            Email = entity.Email,
+            PasswordHash = hash,
+            PasswordSalt = Convert.ToHexString(salt),
             CreatedDate = DateTimeOffset.UtcNow
         };
         
         await _userRepository.CreateAsync(user);
-        var newUser = await _userRepository.GetByIdAsync(user.Id);
 
-        return UserDto.UserToDto(newUser);
+        return UserDto.UserToDto(user);
     }
 
-    public async Task UpdateAsync(UserDto entity)
+    public async Task<UserDto> UpdateAsync(Guid id, UserUpdateDto entity)
     {
-        var user = new User()
-        {
-            Id = entity.Id,
-            Email = entity.Email,
-            Password = entity.Password,
-            FirstName = entity.FirstName,
-            LastName = entity.LastName,
-            CreatedDate = entity.CreatedDate
-        };
+        var oldUserData = await _userRepository.GetByIdAsync(id);
 
-        await _userRepository.UpdateAsync(user);
-    }
-
-    public async Task UpdateAsync(Guid id, UserCreateDto entity)
-    {
-        var oldUserData = await GetByIdAsync(id);
-        
         var user = new User()
         {
             Id = oldUserData.Id,
             Email = entity.Email,
-            Password = entity.Password,
+            PasswordHash = oldUserData.PasswordHash,
+            PasswordSalt = oldUserData.PasswordSalt,
             FirstName = entity.FirstName,
             LastName = entity.LastName,
             CreatedDate = oldUserData.CreatedDate
         };
 
         await _userRepository.UpdateAsync(user);
+
+        return UserDto.UserToDto(user);
     }
+
+    public async Task<UserDto> ChangePassword(Guid id, UserChangePasswordDto entity)
+    {
+        var oldUserData = await _userRepository.GetByIdAsync(id);
+
+        var hash = _passwordService.HashPasword(entity.NewPassword, out var salt);
+
+        var user = new User()
+        {
+            Id = oldUserData.Id,
+            Email = oldUserData.Email,
+            PasswordHash = hash,
+            PasswordSalt = Convert.ToHexString(salt),
+            FirstName = oldUserData.FirstName,
+            LastName = oldUserData.LastName,
+            CreatedDate = oldUserData.CreatedDate
+        };
+
+        await _userRepository.UpdateAsync(user);
+
+        return UserDto.UserToDto(user);
+    }
+
     public async Task DeleteAsync(Guid id)
     {
         await _userRepository.DeleteAsync(id);
