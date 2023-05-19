e989ab817c3ffa798cfc745b7061901649d5c3f7
jaugustyn <kub4a1@gmail.com>
2023-05-20 00:37:23 +0200

    feat: add refresh, revoke and changeRole endpoints

--- AuthAPI/Controllers/AuthController.cs
+++ AuthAPI/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using System.Security.Claims;
 using AuthAPI.Dto;
 using AuthAPI.Models;
 using AuthAPI.Services;
+using Core.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,25 +15,32 @@ namespace AuthAPI.Controllers
     {
         private readonly IUserService _userService;
         private readonly IAccountService _accountService;
+        private readonly IJwtAuthService _jwtAuthService;
 
-        public AuthController( IUserService userService, IAccountService accountService)
+        public AuthController(IUserService userService, IAccountService accountService, IJwtAuthService jwtAuthService)
         {
             _userService = userService;
             _accountService = accountService;
+            _jwtAuthService = jwtAuthService;
         }
         
         [AllowAnonymous]
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginUser user)
+        public async Task<IActionResult> Login([FromBody] UserLoginDto dto)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             
-            var loginResult = await _accountService.Login(user);
-            
-            return loginResult is null ? Unauthorized() : Ok(loginResult);
+            var loginResult = await _accountService.Login(dto);
+
+            if (!loginResult.IsAuthenticated)
+            {
+                return Unauthorized(new { error_message = loginResult.Message });
+            }
+
+            return Ok(loginResult);
         }
         
         [AllowAnonymous]
@@ -49,9 +58,40 @@ namespace AuthAPI.Controllers
 
             return Ok(newUser);
         }
+        
+        [HttpPost]
+        [Route("token/refresh")]
+        public async Task<IActionResult> Refresh(TokenRequestModel token)
+        {
+            if (token is null) return BadRequest( new { error_message = "Invalid client request"});
+            
+            var refreshResult = await _accountService.RefreshToken(token);
 
-        [HttpGet("current")]
-        [Authorize(Policy = "Bearer")]
+            if (refreshResult is null)
+            {
+                return BadRequest(new { error_message = refreshResult.Message });
+            }
+            
+            return Ok(refreshResult);
+        }
+        
+        [HttpPost]
+        [Authorize]
+        [Route("token/revoke")]
+        public async Task<IActionResult> Revoke(TokenRequestModel token)
+        {
+            var isRevoked = await _accountService.RevokeToken(token);
+
+            if (!isRevoked)
+            {
+                return BadRequest(new { error_message = "Token expired."});
+            }
+            
+            return Ok(new { message = "Token revoked." });
+        }
+
+        [HttpGet("currentUser")]
+        [Authorize]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
             var user = await _userService.GetUserByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
@@ -59,6 +99,25 @@ namespace AuthAPI.Controllers
             if (user is null) return NotFound();
             
             return user;
+        }
+        
+        [HttpPost("changeRole")]
+        [Authorize(Policy = "IsAdmin")]
+        public async Task<IActionResult> ChangeUserRole(string email, Role role)
+        {
+            var user = await _userService.GetUserByEmailAsync(email);
+
+            if (user is null) return NotFound(new { error_message = "User does not exist" });
+
+            if (!Enum.IsDefined(typeof(Role), role)) return BadRequest(new { error_message = $"Role '{role}' does not exist" });
+
+            var changed = await _userService.ChangeRole(user.Id, role);
+            if (changed is null)
+            {
+                return Conflict(new { error_message = "Cannot change the role." });
+            }
+            
+            return Ok(changed);
         }
     }
 }
