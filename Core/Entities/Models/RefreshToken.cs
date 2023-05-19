1999f160149c5ddb79782b9d8b91a22d1507b710
jaugustyn <kub4a1@gmail.com>
2023-05-20 00:25:41 +0200

    feat: add roles and refresh token models

---
+++ Core/Entities/Models/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Core.Entities.Models;
+
+namespace AuthAPI.Models;
+
+public class RefreshToken: IEntityBase
+{
+    public Guid Id { get; init; }
+    public string Token { get; set; }
+    public DateTimeOffset Expires { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset? RevokedDate { get; set; }
+    
+    public Guid UserId { get; init; }
+    public User User { get; init; }
+    public bool IsExpired => DateTime.UtcNow >= Expires;
+    public bool IsActive => RevokedDate == null && !IsExpired;
+}
