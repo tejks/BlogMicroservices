e1ab776dc5b5909b1e22ca0be0a1ac6a7d4fee79
jaugustyn <kub4a1@gmail.com>
2023-05-20 00:35:17 +0200

    feat: add refreshtoken generator

--- AuthAPI/Services/JwtAuthService.cs
+++ AuthAPI/Services/JwtAuthService.cs
@@ -1,6 +1,8 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using AuthAPI.Models;
 using Core.Entities.Models;
 using Microsoft.IdentityModel.Tokens;
 
@@ -15,7 +17,7 @@ public class JwtAuthService : IJwtAuthService
         _config = config;
     }
     
-    public string GenerateToken(User user)
+    public string GenerateAccessToken(User user)
     {
         var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Secret"]));
         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
@@ -23,7 +25,8 @@ public class JwtAuthService : IJwtAuthService
         
         var claims = new[]
         {
-            new Claim(ClaimTypes.Email, user.Email)
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("role", Enum.GetName(user.Role))
         };
 
         var token = new JwtSecurityToken(
@@ -37,5 +40,50 @@ public class JwtAuthService : IJwtAuthService
         var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
         
         return tokenString;
+    }
+
+    public RefreshToken GenerateRefreshToken(User user)
+    {
+        var randomNumber = new byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomNumber);
+            
+            return new RefreshToken
+            {
+                UserId = user.Id,
+                Token = Convert.ToBase64String(randomNumber),
+                Expires = DateTime.UtcNow.AddDays(10),
+                CreatedDate = DateTime.UtcNow,
+            };
+        }
+    }
+    
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var key = Encoding.UTF8.GetBytes(_config["JwtSettings:Secret"]);
+
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ClockSkew = TimeSpan.Zero
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        SecurityToken securityToken;
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+        
+        return principal;
     }
 }
