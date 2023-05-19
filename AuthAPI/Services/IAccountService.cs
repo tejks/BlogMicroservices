0542d1bc9e75218291f2003c556ad4a7a93f364b
jaugustyn <kub4a1@gmail.com>
2023-05-20 00:36:50 +0200

    feat: reimplement login method, add refresh and revoke token

--- AuthAPI/Services/IAccountService.cs
+++ AuthAPI/Services/IAccountService.cs
@@ -5,8 +5,10 @@ namespace AuthAPI.Services
 {
     public interface IAccountService
     {
-        Task<AuthTokenResponse?> Login(LoginUser loginUser);
+        Task<UserAuthResponse> Login(UserLoginDto userLoginDto);
         Task<UserDto?> Register(UserCreateDto userCreateDto);
         Task<UserDto?> ChangePassword(Guid id, UserChangePasswordDto userChangePasswordDto);
+        Task<UserAuthResponse> RefreshToken(TokenRequestModel tokenRequestModel);
+        Task<bool> RevokeToken(TokenRequestModel tokenRequestModel);
     }
 }
