512dabb02fd9e8db65bac78c02b6bdbf2ecc47e0
Tejks <sarmatys.programmer@gmail.com>
2023-05-25 12:23:01 +0200

    feat: create message bus subscriber for comment api

---
+++ CommentsAPI/AsyncDataService/MessageBusCommentSubscriber.cs
@@ -0,0 +1,19 @@
+﻿using CommentsAPI.EventProcessing;
+using Infrastructure.AsyncDataServices;
+
+namespace CommentsAPI.AsyncDataService;
+
+public class MessageBusCommentSubscriber : MessageBusSubscriber
+{
+    private readonly IEventProcessor _eventProcessor;
+    
+    public MessageBusCommentSubscriber(IEventProcessor eventProcessor)
+    {
+        _eventProcessor = eventProcessor;
+    }
+
+    protected override void ProcessEvent(string message)
+    {
+        _eventProcessor.ProcessEvent(message);
+    }
+}
