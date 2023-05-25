8ae435a636cb1e662222f9c55eb5a66d1386917e
Tejks <sarmatys.programmer@gmail.com>
2023-05-25 12:22:16 +0200

    feat: create message bus client abstract class

---
+++ PostsAPI/AsyncDataService/MessageBusPostClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Infrastructure.AsyncDataServices;
+using Infrastructure.AsyncDataServices.Dto;
+
+namespace PostsAPI.AsyncDataService;
+
+public class MessageBusPostClient : MessageBusClient, IMessageBusPostClient
+{
+    public MessageBusPostClient(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public void PublishPostDeleteEvent(PostDeletedPublisherDto dto)
+    {
+        var message = JsonSerializer.Serialize(dto);
+
+        if (Connection.IsOpen)
+        {
+            Console.WriteLine("--> RabbitMQ connection open, sending message...");
+            SendMessage(message);
+        }
+        else
+        {
+            Console.WriteLine("--> RabbitMQ connection closed, not sending");
+        }
+    }
+}
