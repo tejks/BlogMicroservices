068986aaca01fc5df169ebf2fe067bc21a3a3331
Tejks <sarmatys.programmer@gmail.com>
2023-05-25 12:21:54 +0200

    feat: create message bus subscriber abstract class

---
+++ Infrastructure/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Infrastructure.AsyncDataServices;
+
+public abstract class MessageBusSubscriber : BackgroundService
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    private readonly string _queueName;
+
+    protected MessageBusSubscriber()
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@rabbitmq:5672/")
+        };
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+        _queueName = _channel.QueueDeclare().QueueName;
+        _channel.QueueBind(queue: _queueName,
+            exchange: "trigger",
+            routingKey: "");
+
+        Console.WriteLine("--> Listening on the Message Bus...");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(_channel);
+
+        consumer.Received += (moduleHandle, ea) =>
+        {
+            Console.WriteLine("--> Event Received!");
+            var body = ea.Body;
+            var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+            
+            ProcessEvent(notificationMessage);
+        };
+
+        _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    protected abstract void ProcessEvent(string message);
+
+    public override void Dispose()
+    {
+        if (_channel.IsOpen)
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+
+        base.Dispose();
+    }
+}
