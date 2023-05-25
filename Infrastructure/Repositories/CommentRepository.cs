9098f50f984bab09c0ebd58ecbe1b41528efb5e2
Tejks <sarmatys.programmer@gmail.com>
2023-05-25 12:19:09 +0200

    feat: add methods to comment repository

--- Infrastructure/Repositories/CommentRepository.cs
+++ Infrastructure/Repositories/CommentRepository.cs
@@ -1,14 +1,29 @@
 ﻿using Core.Entities.Models;
 using Core.Repositories;
 using Infrastructure.Data;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Infrastructure.Repositories;
 
 public class CommentRepository : GenericRepository<Comment>, ICommentRepository
 {
+    private readonly FilterDefinitionBuilder<Comment> _filterBuilder = Builders<Comment>.Filter;
+    private readonly IMongoCollection<Comment> _collection;
+    
     public CommentRepository(IMongoDbContext context): base(context)
     {
+        _collection = context.Comments;
+    }
+    
+    public IEnumerable<Comment> GetCommentsByPostIdSync(Guid postId)
+    {
+        var filter = _filterBuilder.Eq(x => x.PostId, postId);
+        return _collection.Find(filter).ToList();
+    }
+
+    public void DeleteCommentSync(Guid id)
+    {
+        var filter = _filterBuilder.Eq("_id", id);
+        _collection.DeleteOne(filter);
     }
 }
